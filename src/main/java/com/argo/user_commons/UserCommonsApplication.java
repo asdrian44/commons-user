24a25cf8b64f0c586a0f45735594fdda472c9761
Adrian Galindo <asdrian4455@hotmail.com>
2020-11-13 15:09:10 -0500

    nuevogaaaa

--- src/main/java/com/argo/user_commons/UserCommonsApplication.java
+++ src/main/java/com/argo/user_commons/UserCommonsApplication.java
@@ -2,7 +2,13 @@ package com.argo.user_commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
 @SpringBootApplication
 public class UserCommonsApplication {
 
